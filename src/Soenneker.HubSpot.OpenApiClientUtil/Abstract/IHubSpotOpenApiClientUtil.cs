7980cdda7371862063854e69678e256dfaaeb1d3
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:18 -0500

    xml commenting

--- src/Soenneker.HubSpot.OpenApiClientUtil/Abstract/IHubSpotOpenApiClientUtil.cs
+++ src/Soenneker.HubSpot.OpenApiClientUtil/Abstract/IHubSpotOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.HubSpot.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IHubSpotOpenApiClientUtil : IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<HubSpotOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
